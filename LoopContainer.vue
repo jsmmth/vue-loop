29a393f22a6bbee761db0ec25d3713db752badfd
Joseph Smith <joe@layerful.com>
2017-04-28 18:38:17 +0100

    Initial setup

---
+++ LoopContainer.vue
@@ -0,0 +1,24 @@
+<template>
+	<div :class="['loop-container', {'full': full, 'horizontal': horizontal}]">
+		<slot></slot>
+	</div>
+</template>
+
+<script>
+export default {
+
+	props: {
+		full: {
+			type: Boolean,
+			default: true
+		},
+
+		horizontal: {
+			type: Boolean,
+			default: false
+		}
+	}
+
+
+}
+</script>
